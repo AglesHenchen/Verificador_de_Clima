5fcf924b67e6d30e19e2789b1d923af676e70524
Agles Henchen <158245769+AglesHenchen@users.noreply.github.com>
2025-04-17 17:36:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function coordenadaCidade(cidade) {
-    return fetch(`https://nominatim.openstreetmap.org/search?city=${cidade}&format=json`)
+function coordenadaCidade(pais, estado, cidade) {
+    return fetch(`https://nominatim.openstreetmap.org/search?city=${cidade}&state=${estado}&country=${pais}&format=json`)
         .then(response => response.json())
         .then(data => {
             if (data && data[0]) {
@@ -12,51 +12,91 @@ function coordenadaCidade(cidade) {
         });
 }
 
-function verificarClima() {
-    let cidade = document.getElementById("cidade").value.trim();
-    if (!cidade) {
-        alert("Informe o nome cidade para realizar a verificação");
+function procurarCoordenadas(latitude, longitude) {
+    return fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.address) {
+                const cidade = data.address.city || data.address.town || data.address.village;
+                return cidade;
+            } else {
+                throw new Error('Endereço não encontrado');
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao buscar cidade:', error);
+            return null;
+        });
+}
+
+async function verificarClima() {
+    let pais_input = document.getElementById("pais").value.trim();
+    let estado_input = document.getElementById("estado").value.trim();
+    let cidade_input = document.getElementById("cidade").value.trim();
+
+    if (!pais_input || !estado_input || !cidade_input) {
+        alert("Necessário preencher todos os campos para realizar a verificação");
         return;
     }
 
     document.getElementById("resultado").style.display = "none";
-    document.getElementById('erro').textContent = '';
+    document.getElementById("erro").style.display = "none";
 
-    coordenadaCidade(cidade)
-        .then(coordenadas => {
-            const { latitude, longitude } = coordenadas;
+    try {
+        const coordenadas = await coordenadaCidade(pais_input, estado_input, cidade_input);
+        const { latitude, longitude } = coordenadas;
 
-            const url = "https://api.open-meteo.com/v1/forecast";
-            const parametros = {
-                latitude: latitude,
-                longitude: longitude,
-                current_weather: true,
-                temperature_unit: 'celsius'
-            };
+        const cidade_resultado = await procurarCoordenadas(latitude, longitude);
 
-            const parametrosQuery = new URLSearchParams(parametros).toString();
-            const api = `${url}?${parametrosQuery}`;
+        const parametros = {
+            latitude: latitude,
+            longitude: longitude,
+            current_weather: true,
+            temperature_unit: 'celsius'
+        };
 
-            fetch(api)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.current_weather) {
-                        let temperatura = data.current_weather.temperature;
-                        document.getElementById('temperatura').innerHTML = "Temperatura:";
-                        document.getElementById('temperatura').innerHTML += "<br>";
-                        document.getElementById('temperatura').innerHTML += `${temperatura}°C`;
-                        document.getElementById('resultado').style.display = 'block';
-                    } else {
-                        document.getElementById('erro').textContent = 'Não foi possível obter a temperatura atual.';
-                    }
-                })
-                .catch(error => {
-                    document.getElementById('erro').textContent = 'Erro ao obter dados meteorológicos: ' + error.message;
-                });
-        })
-        .catch(error => {
-            document.getElementById('erro').textContent = 'Erro ao obter as coordenadas: ' + error.message;
-        });
+        const parametrosQuery = new URLSearchParams(parametros).toString();
+        const api = `https://api.open-meteo.com/v1/forecast?${parametrosQuery}`;
+
+        const response = await fetch(api);
+        const data = await response.json();
+
+        if (data.current_weather) {
+            let temperatura = data.current_weather.temperature;
+            if (temperatura <= 20) { 
+                //frio
+                document.getElementById("resultado").style.display = "block";
+                document.getElementById("resultado").style.padding = "5px";
+                document.getElementById("resultado").style.backgroundColor = "rgb(140, 178, 247)";
+                document.getElementById("temperatura").innerHTML = `${cidade_resultado} <br>`;
+                document.getElementById("temperatura").innerHTML += `${parseInt(temperatura)} °C <br>`;
+                document.getElementById("temperatura").innerHTML += `❄️`;
+            }
+            else if (temperatura >= 30) {
+                //quente
+                document.getElementById("resultado").style.display = "block";
+                document.getElementById("resultado").style.padding = "5px";
+                document.getElementById("resultado").style.backgroundColor = "rgb(247, 179, 140)";
+                document.getElementById("temperatura").innerHTML = `${cidade_resultado} <br>`;
+                document.getElementById("temperatura").innerHTML += `${parseInt(temperatura)} °C <br>`;
+                document.getElementById("temperatura").innerHTML += `☀️`;
+            }
+            else {
+                //ameno
+                document.getElementById("resultado").style.display = "block";
+                document.getElementById("resultado").style.padding = "5px";
+                document.getElementById("resultado").style.backgroundColor = "rgb(208, 214, 216)";
+                document.getElementById("temperatura").innerHTML = `${cidade_resultado} <br>`;
+                document.getElementById("temperatura").innerHTML += `${parseInt(temperatura)} °C <br>`;
+                document.getElementById("temperatura").innerHTML += `⛅️`;
+            }
+
+        } else {
+            document.getElementById("erro").style.display = "block";
+            document.getElementById("erro").innerHTML = "Não foi possível obter a temperatura atual.";
+        }
+    } catch (error) {
+        document.getElementById("erro").style.display = "block";
+        document.getElementById("erro").innerHTML = "Erro: " + error.message;
+    }
 }
-
-
